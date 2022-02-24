4610281a5b84ddf08d4fcde349dc8a1b04ba6bde
Diego Esteban Cortes Escandón <e-dcortes@autopia.cl>
2022-02-24 14:33:21 -0300

    Avances correción de padding general y componente card junto con sección recents posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Jumbotron from "../components/jumbotron"
+import RecentPosts from "../components/recent-posts"
 import "../styles/index.css"
 
 const IndexPage = () => (
-  <div className="wrapper">
+  <div>
     <Jumbotron />
+    <RecentPosts />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </p>
