b9f2b33cc318b35a0af6baa7d697a8118ab1ac93
Diego Esteban Cortes Escandón <e-dcortes@autopia.cl>
2022-02-25 01:14:25 -0300

    Ajustes de estilos varios, junto con nuevas secciones en la pag principal

--- src/components/recent-posts.js
+++ src/components/recent-posts.js
@@ -12,6 +12,22 @@ const RecentPosts = () => {
           </h3>
           <div className="recent-post__cards__card__subtitle">
             <p>12 Feb 2020</p>
+            <p>|</p>
+            <p>Design, Pattern</p>
+          </div>
+          <p className="recent-post__cards__card__text">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+        <div className="recent-post__cards__card">
+          <h3 className="recent-post__cards__card__title">
+            Making a design system from scratch
+          </h3>
+          <div className="recent-post__cards__card__subtitle">
+            <p>12 Feb 2020</p>
+            <p>|</p>
             <p>Design, Pattern</p>
           </div>
           <p className="recent-post__cards__card__text">
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import { Link } from "gatsby"
 import Jumbotron from "../components/jumbotron"
 import RecentPosts from "../components/recent-posts"
 import "../styles/index.css"
+import FeatureWorks from "../components/feature-works"
 
 const IndexPage = () => (
   <div>
     <Jumbotron />
     <RecentPosts />
+    <FeatureWorks />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
     </p>
